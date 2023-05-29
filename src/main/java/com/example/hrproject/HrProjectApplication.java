d09727e5bc0715582c129613bab0f32c81d123b5
kunsanglee <gosmdochee2@gmail.com>
2023-05-29 23:46:55 +0900

    #2 - Sequence diagram 작성
    1. 특정 사원의 현재 정보 조회
    2. 특정 사원의 이력 정보 조회
    3. 부서 및 위치 정보 조회
    4. 특정 부서의 급여를 특정 비율로 인상 및 사원 정보 업데이트

--- src/main/java/com/example/hrproject/HrProjectApplication.java
+++ src/main/java/com/example/hrproject/HrProjectApplication.java
@@ -3,6 +3,7 @@ package com.example.hrproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class HrProjectApplication {
 
